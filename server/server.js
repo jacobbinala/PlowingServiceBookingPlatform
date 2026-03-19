856ad8225b53e5b16796708720eabb00102b8a0f
IKENNA Divine <ikennadivine17@gmail.com>
2026-03-19 14:16:27 -0400

    update .env.example and server.js to change the default port from 5000 to 5001

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
